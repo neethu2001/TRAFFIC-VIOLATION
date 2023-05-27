91cb94b369b17865292f53c0dc16195fa250b3de
Neethu.M <55880316+neethu2001@users.noreply.github.com>
2023-05-27 15:20:57 +0530

    Update yolo_image_hel.py

--- yolo_image_hel.py
+++ yolo_image_hel.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import is_centroid_inside
 def detect_hel():
     net = cv2.dnn.readNet("yolov4-custom_10000.weights", "yolov4-custom.cfg")
     print("done")
@@ -11,7 +11,7 @@ def detect_hel():
         new_height = int(new_width * aspect_ratio)
         return cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
 
-    # Define the text font and size
+
 
 
     classes = []
@@ -23,14 +23,7 @@ def detect_hel():
     layer_names = net.getLayerNames()
     output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
 
-    img = cv2.imread('img.jpg')
-
-    height, width, channels = img.shape
-
-    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-
-    net.setInput(blob)
-    outs = net.forward(output_layers)
+    
 
     class_ids = []
     confidences = []
@@ -44,18 +37,7 @@ def detect_hel():
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
-                h = int(detection[3] * height)
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.4)
-
-    violations = []
-    triple_violations = []
-    for i in range(len(boxes)):
+    
         if i in indexes:
             x, y, w, h = boxes[i]
             label = classes[class_ids[i]]
@@ -73,14 +55,16 @@ def detect_hel():
                 if helmet_count > 2:
                     triple_violations.append(i)
 
-            # Draw all detected objects
+           
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
+            with open('report.csv','w'):
+                csv.writer(place,count,violation,date,time)
 
 
 
 
-      # Highlight helmet violations
+    
     for (i, j) in violations:
         x, y, w, h = boxes[i]
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
@@ -96,13 +80,6 @@ def detect_hel():
     
 
 
-def is_centroid_inside(x1, y1, w1, h1, x2, y2, w2, h2):
-    centroid_x2 = x2 + w2 // 2
-    centroid_y2 = y2 + h2 // 2
-    
-    x_in_range = x1 <= centroid_x2 and centroid_x2 <= x1 + w1
-    y_in_range = y1 <= centroid_y2 and centroid_y2 <= y1 + h1
-    
-    return x_in_range and y_in_range
+
 
 
