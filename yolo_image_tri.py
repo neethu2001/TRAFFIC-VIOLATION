20ade414cad70f7ce5cb168f24e93bb226129a24
Neethu.M <55880316+neethu2001@users.noreply.github.com>
2023-05-27 15:23:51 +0530

    Update yolo_image_tri.py

--- yolo_image_tri.py
+++ yolo_image_tri.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from anpr_video import anpr_detect
+import is_inside_centroid
 
 def detect_tri():
     net = cv2.dnn.readNet("yolov4-custom_10000.weights", "yolov4-custom.cfg")
@@ -46,41 +47,18 @@ def detect_tri():
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
-
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.4)
-
-    violations = []
-    triple_violations = []
-    for i in range(len(boxes)):
-        if i in indexes:
-            x, y, w, h = boxes[i]
-            label = classes[class_ids[i]]
-            if label == 'bike':
-                helmet_count = 0
-                for j in range(len(boxes)):
-                    if j in indexes and i != j:
-                        x2, y2, w2, h2 = boxes[j]
-                        label2 = classes[class_ids[j]]
-                        if label2 in ['hel', 'nohel'] and is_centroid_inside(x, y, w, h, x2, y2, w2, h2):
-                            helmet_count += 1
-                            if label2 == 'nohel':
-                                violations.append((i, j))
+        
 
                 if helmet_count > 2:
                     triple_violations.append(i)
 
-            # Draw all detected objects
+ 
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1)
 
  
 
-    # Highlight triple violations
+
     for i in triple_violations:
         x, y, w, h = boxes[i]
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
@@ -88,6 +66,8 @@ def detect_tri():
         cv2.imwrite(f"violation_bike_{i}.png", cropped_bike)
         anpr_detect(f"violation_bike_{i}.png")
         cv2.putText(img, "Triple Violation", (x + 20, y + 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+         with open('report.csv','w'):
+                csv.writer(place,count,violation,date,time)
 
     cv2.imshow("Output", img)
     cv2.waitKey(0)
@@ -95,13 +75,5 @@ def detect_tri():
     cv2.imwrite("pr.jpg", img)
 
 
-def is_centroid_inside(x1, y1, w1, h1, x2, y2, w2, h2):
-    centroid_x2 = x2 + w2 // 2
-    centroid_y2 = y2 + h2 // 2
-    
-    x_in_range = x1 <= centroid_x2 and centroid_x2 <= x1 + w1
-    y_in_range = y1 <= centroid_y2 and centroid_y2 <= y1 + h1
-    
-    return x_in_range and y_in_range
 
 
